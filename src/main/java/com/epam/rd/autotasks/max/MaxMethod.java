247e7d6faa35317cf6bf846c74dae6cd1147baf7
firdavs <firdavsolimjonov25@gmail.com>
2022-12-13 10:24:40 +0500

    added

--- src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
+++ src/main/java/com/epam/rd/autotasks/max/MaxMethod.java
@@ -2,6 +2,11 @@ package com.epam.rd.autotasks.max;
 
 public class MaxMethod {
     public static int max(int[] values) {
-        throw new UnsupportedOperationException();
+        int max = values[0];
+
+        for (int value : values)
+            if (max<value) max = value;
+
+        return max;
     }
 }
